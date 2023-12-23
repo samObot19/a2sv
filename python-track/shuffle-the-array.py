c083413d20cd1047023bc6acc5d31de33adb350a
samObot19 <125662080+samObot19@users.noreply.github.com>
2023-12-23 04:38:53 -0800

    Added solution for Shuffle the Array

---
+++ python-track/shuffle-the-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        
+        i = 0
+        j = n
+        
+        ans = []
+        while (j < 2*n):
+            ans.append(nums[i])
+            ans.append(nums[j])
+            i += 1
+            j += 1
+            
+        return ans
