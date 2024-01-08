3eba643772369d304bb62bb8b56466a55c11457b
samObot19 <125662080+samObot19@users.noreply.github.com>
2024-01-08 11:56:52 -0800

    Added solution for Max Consecutive Ones III

---
+++ Progress_sheet_problems /max-consecutive-ones-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0;
+        int j = 0;
+        while(i < nums.size()){
+            if(nums[i] == 0 ) k--;
+            if(k < 0){
+                if(nums[j] == 0) k++;
+                j++;
+            }
+            i++;
+        }
+
+        return i - j;
+        
+    }
+};
